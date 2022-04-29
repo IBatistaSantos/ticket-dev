675ff52a28b1d8e4c7768aa8789b673176209460
Israel Batista <israelbatiista19@gmail.com>
2022-04-29 12:32:40 -0300

    add lib faker from unit test

--- tests/infra/prisma/repositories/account-repository.spec.ts
+++ tests/infra/prisma/repositories/account-repository.spec.ts
@@ -1,6 +1,8 @@
-import { AccoutRepository } from '@/infra/db/prisma/repositories/account-repository';
 import { DeepMockProxy, mockDeep, mockReset } from 'jest-mock-extended';
 import { PrismaClient, User } from '@prisma/client';
+import { faker } from '@faker-js/faker';
+
+import { AccoutRepository } from '@/infra/db/prisma/repositories/account-repository';
 import prisma from '@/infra/db/prisma/client';
 
 jest.mock('@/infra/db/prisma/client', () => ({
@@ -11,49 +13,44 @@ jest.mock('@/infra/db/prisma/client', () => ({
 describe('AccoutRepository - Prisma', () => {
   let sut: AccoutRepository;
   let prismaMock: DeepMockProxy<PrismaClient>;
+  let account: User;
 
   beforeAll(() => {
     prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>;
+    account = {
+      id: faker.random.alpha(),
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password: faker.random.alpha(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
   });
 
   beforeEach(async () => {
     mockReset(prismaMock);
     sut = new AccoutRepository(prismaMock);
 
-    const account: User = {
-      id: 'anyId',
-      name: 'anyName',
-      email: 'anyEmail',
-      password: 'anyPassword',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
-
     prismaMock.user.findUnique.mockResolvedValue(account);
     prismaMock.user.create.mockResolvedValue(account);
   });
 
   it('should return account when load returning account', async () => {
-    const result = await sut.load({ email: 'anyEmail' });
+    const result = await sut.load({ email: account.email });
     expect(result).toHaveProperty('id');
-    expect(result?.email).toEqual('anyEmail');
+    expect(result?.email).toEqual(account.email);
   });
 
   it('should return null when load returning null', async () => {
     prismaMock.user.findUnique.mockResolvedValueOnce(null);
-    const result = await sut.load({ email: 'anyEmail' });
+    const result = await sut.load({ email: account.email });
     expect(result).toBe(null);
   });
 
   it('should create new account', async () => {
-    const data = {
-      name: 'anyName',
-      email: 'anyEmail',
-      password: 'anyPassword',
-    };
-    const result = await sut.createAccount(data);
+    const result = await sut.createAccount(account);
 
     expect(result).toHaveProperty('id');
-    expect(result?.email).toEqual('anyEmail');
+    expect(result?.email).toEqual(account.email);
   });
 });
